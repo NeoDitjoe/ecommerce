f1d69662ca677a4aeef8bedf091e161f7e6eaa55
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-02-02 11:14:55 +0200

    CALLBACKS

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -16,6 +16,12 @@ export default NextAuth({
     }),
   ],
 
+  callbacks: {
+    async signIn({ account, profile }) {
+      return true // Do different verification for other providers that don't have `email_verified`
+    },
+  },
+
   secret: process.env.SECRET,
 })
 
