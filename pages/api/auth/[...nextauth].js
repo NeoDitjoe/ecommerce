2e07f4a22b7a4486012bfd63a06dbc325213778e
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-02-02 06:00:34 +0200

    added other auth providers

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,23 +1,23 @@
 import NextAuth from 'next-auth'
-// import AppleProvider from 'next-auth/providers/apple'
-// import FacebookProvider from 'next-auth/providers/facebook'
+import AppleProvider from 'next-auth/providers/apple'
+import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
 // import EmailProvider from 'next-auth/providers/email'
 
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
-    // AppleProvider({
-    //   clientId: process.env.APPLE_ID,
-    //   clientSecret: process.env.APPLE_SECRET
-    // }),
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_ID,
-    //   clientSecret: process.env.FACEBOOK_SECRET
-    // }),
+    AppleProvider({
+      clientId: process.env.APPLE_ID,
+      clientSecret: process.env.APPLE_SECRET
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET
+    }),
     GoogleProvider({
-      clientId: '534139976513-87qeev7om3ibrjdprbl7aq2td501egje.apps.googleusercontent.com',
-      clientSecret: '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'
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_KEY
     }),
     // Passwordless / email sign in
     // EmailProvider({
