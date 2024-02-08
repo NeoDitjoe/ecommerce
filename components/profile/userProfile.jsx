af1e0135d457da14d70cddc6816774128b8c580d
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-02-08 16:21:23 +0200

    change profile image and password

--- components/profile/userProfile.jsx
+++ components/profile/userProfile.jsx
@@ -1,25 +1,65 @@
 import addItem from "@/lib/database/addItems"
 import { useSession } from "next-auth/react"
-import { useRef } from "react"
+import { useRef, useState } from "react"
+import { Item } from "../dashBoard/muiStyle"
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+import style from './userProfile.module.css'
 
 export default function UserProfile() {
-  const imageRef = useRef()
+  const passwordRef = useRef()
+  const [imageFile, setImageFile] = useState(null)
+  const [showPassword, setShowPassword] = useState(false)
+
   const { data: session } = useSession()
 
-  const userEmail = session && session.user.email[0]
+  const userEmail = session && session.user.email
 
   const upload = async function () {
-    const image = imageRef.current.value
+    const password = passwordRef.current.value
 
-    await addItem('/api/profile/user', {userEmail: userEmail, image: image})
+    await addItem('/api/profile/user', { userEmail: userEmail && userEmail[0], image: imageFile, password: password })
+  }
+
+  function convertToBase64(e) {
+
+    var reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = () => {
+      setImageFile(reader.result)
+    }
+    reader.onerror = error => {
+      console.log('Error: ', error)
+    }
   }
 
   return (
-    <div>
-      <form>
-        <input type="text" name='image'ref={imageRef}/>
-      </form>
-        <button onClick={upload} >submit</button>
+    <div className={style.main}>
+      <h3>{userEmail && userEmail[1]}</h3>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+
+          <Grid xs={12} md={4} s={0} >
+            <Item className={style.item}>
+
+              <label>Profile Image file: </label>
+              <input type="file" name='image' onChange={convertToBase64} />
+              <br />
+
+              <label>Change Password: </label>
+              <div className={style.passwordInput}>
+                <input type={showPassword ? 'text' : 'password'} name='image' ref={passwordRef} />
+                <p onClick={() => setShowPassword(!showPassword)}>{showPassword ? 'Hide': 'Show'} </p>
+              </div>
+              <br />
+
+              <button className={style.button} onClick={upload} >submit</button>
+            </Item>
+          </Grid>
+
+        </Grid>
+      </Box>
+
     </div>
   )
 }
--- pages/api/profile/user.js
+++ pages/api/profile/user.js
@@ -4,10 +4,10 @@ import UserProfile from "@/lib/database/profile/userProfile"
 export default async function handler(req, res){
 
   if(req.method === 'POST'){
-    const { userEmail, image} = req.body
+    const { userEmail, image, password} = req.body
 
     try {
-      await UserProfile(userEmail, image)
+      await UserProfile(userEmail, image, password)
       res.status(200).json({ message: 'success' })
     } catch (error) {
       res.status(417).json({ message: 'failed attempt' })
